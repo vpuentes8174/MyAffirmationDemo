9ae03569f5c4d1e2f7c4250e179f8d4917e80b0b
Vanessa <Princessvanessa9977@gmail.com>
2019-07-02 11:33:10 -0500

    Initial Commit

---
+++ MyAffirmationDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyAffirmationDemo
+//
+//  Created by SER-YCL9 on 7/2/19.
+//  Copyright © 2019 Vanessa Puentes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
